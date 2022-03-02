5cb7eff99393c0ade8a711e1892978c55fab0ded
Kevin <kevin.svanback@gmail.com>
2022-03-02 11:20:21 +0100

    typewriter effect & background image

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
+import styles from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <section className={styles.homeSection}>
+      <div className={styles.typewriter}>
+        <p>I am Kevin the </p> <Typewriter options={{
+          strings: ['frontend developer'],
+          autoStart: true,
+          loop: true,
+        }} />
+      </div>
+    </section>
   );
 };
 
