6bc53ccdc3337120e49fc13df8c25d8a6e8b73bd
Kevin <kevin.svanback@gmail.com>
2022-03-04 12:17:56 +0100

    projetcs displayed

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -3,12 +3,32 @@ import styles from './ProjectsPage.module.css';
 
 const ProjectsPage = () => {
   return (
-    <div className={styles.body}>
-      <div className={styles.projectContainer}>
-        <div className={`${styles.project} ${styles.project1}`}></div>
-        <div className={`${styles.project} ${styles.project2}`}></div>
+    <>
+      <div className={styles.projectsContainer}>
+        <div className={styles.projectCard}>
+          <div className={`${styles.project} ${styles.project1}`}></div>
+          <div className={styles.projectInfoContainer}>
+            <h1>Crypto Tracker</h1>
+            <p>React crypto tracker app displays cryptocurrency prices by market cap.</p>
+            <div className={styles.buttonContainer}>
+              <button>Live Demo</button>
+              <button>Source Code</button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.projectCard}>
+          <div className={`${styles.project} ${styles.project2}`}></div>
+          <div className={styles.projectInfoContainer}>
+            <h1>Food Order App</h1>
+            <p>React food order app.</p>
+            <div className={styles.buttonContainer}>
+              <button>Live Demo</button>
+              <button>Source Code</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
