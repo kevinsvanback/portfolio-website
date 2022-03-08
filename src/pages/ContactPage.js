933e6c97768e3e9458b765d931f64171d83cd2d3
Kevin <kevin.svanback@gmail.com>
2022-03-08 21:01:10 +0100

    initialize ContactPage

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -4,7 +4,25 @@ import styles from './ContactPage.module.css';
 const ContactPage = () => {
   return (
     <>
-      <div className={styles.contactPage}>ContactPage</div>
+      <section className={styles.contactSection}>
+        <div className={styles.contactContainer}>
+          <div className={styles.contactInfo}>
+            {/* <i class="fas fa-envelope fa-2x"></i> */}
+            <h3>Email</h3>
+            <p>kevin.svanback@gmail.com</p>
+          </div>
+          <div className={styles.contactInfo}>
+            {/* <i class="fas fa-phone fa-2x"></i> */}
+            <h3>Phone</h3>
+            <p>076-3060772</p>
+          </div>
+          <div className={styles.contactInfo}>
+            {/* <i class="fas fa-address-card fa-2x"></i> */}
+            <h3>Address</h3>
+            <p>Apelvägen 9, 619 91 Trosa</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
