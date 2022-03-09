975d6390ea2fb2530669bada149f4936450fb559
Kevin <kevin.svanback@gmail.com>
2022-03-09 13:43:09 +0100

    ContactPage email/phone

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,26 +1,72 @@
-import React from 'react';
+import { faChevronDown, faChevronUp, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import styles from './ContactPage.module.css';
 
 const ContactPage = () => {
+  const [emailIsShown, setEmailIsShown] = useState(false);
+  const [phoneIsShown, setPhoneIsShown] = useState(false);
+  // const [addressIsShown, setAddressIsShown] = useState(false);
+
+  const showEmailHandler = () => {
+    setEmailIsShown(!emailIsShown);
+  };
+  const showPhoneHandler = () => {
+    setPhoneIsShown(!phoneIsShown);
+  };
+  // const showAddressHandler = () => {
+  //   setAddressIsShown(!addressIsShown);
+  // };
+
   return (
     <>
       <section className={styles.contactSection}>
         <div className={styles.contactContainer}>
           <div className={styles.contactInfo}>
-            {/* <i class="fas fa-envelope fa-2x"></i> */}
-            <h3>Email</h3>
-            <p>kevin.svanback@gmail.com</p>
+            <div className={styles.buttonAndHeadingContainer}>
+              <div className={styles.chevronButton} onClick={showEmailHandler}>
+                <FontAwesomeIcon className={styles.chevronIcon} icon={!emailIsShown ? faChevronDown : faChevronUp} />
+              </div>
+              <div className={styles.contactHeading}>
+                <h3>Email</h3>
+                <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
+              </div>
+            </div>
+            <div className={styles.contactContent}>
+              {!emailIsShown && <p className={styles.invisible}>kevin.svanback@gmail.com</p>}
+              {emailIsShown && <p>kevin.svanback@gmail.com</p>}
+            </div>
           </div>
           <div className={styles.contactInfo}>
-            {/* <i class="fas fa-phone fa-2x"></i> */}
-            <h3>Phone</h3>
-            <p>076-3060772</p>
+            <div className={styles.buttonAndHeadingContainer}>
+              <div className={styles.chevronButton} onClick={showPhoneHandler}>
+                <FontAwesomeIcon className={styles.chevronIcon} icon={!phoneIsShown ? faChevronDown : faChevronUp} />
+              </div>
+              <div className={styles.contactHeading}>
+                <h3>Phone</h3>
+                <FontAwesomeIcon className={styles.icon} icon={faPhone} />
+              </div>
+            </div>
+            <div className={styles.contactContent}>
+              {!phoneIsShown && <p className={styles.invisible}>076-3060772</p>}
+              {phoneIsShown && <p>076-3060772</p>}
+            </div>
           </div>
-          <div className={styles.contactInfo}>
-            {/* <i class="fas fa-address-card fa-2x"></i> */}
-            <h3>Address</h3>
-            <p>Apelvägen 9, 619 91 Trosa</p>
-          </div>
+          {/* <div className={styles.contactInfo}>
+            <div className={`${styles.buttonAndHeadingContainer} ${styles.lastItem}`}>
+              <div className={styles.chevronButton} onClick={showAddressHandler}>
+                <FontAwesomeIcon className={styles.chevronIcon} icon={!addressIsShown ? faChevronDown : faChevronUp} />
+              </div>
+              <div className={styles.contactHeading}>
+                <h3>Address</h3>
+                <FontAwesomeIcon className={styles.icon} icon={faAddressCard} />
+              </div>
+            </div>
+            <div className={styles.contactContent}>
+              {!addressIsShown && <p className={styles.invisible}>Apelvägen 9, Trosa</p>}
+              {addressIsShown && <p>Apelvägen 9, Trosa</p>}
+            </div>
+          </div> */}
         </div>
       </section>
     </>
