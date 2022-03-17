314cc5514b73753521c68e9ddcd56ce5a340563a
Kevin <kevin.svanback@gmail.com>
2022-03-17 12:52:50 +0100

    image resize + chat room project

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -18,6 +18,17 @@ const ProjectsPage = () => {
       <div className={styles.projectCard}>
         <div className={`${styles.project} ${styles.project2}`}></div>
         <div className={styles.projectInfoContainer}>
+          <h1>Chat Room</h1>
+          <p>React chat room app with Google authentication.</p>
+          <div className={styles.buttonContainer}>
+            <a href={'https://kevinsvanback-react-chat-room.netlify.app/'} target='_blank' rel='noopener noreferrer' className={styles.button}>Live Demo</a>
+            <a href={'https://github.com/kevinsvanback/chat-room-app'} target='_blank' rel='noopener noreferrer' className={styles.button}>Source Code</a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.projectCard}>
+        <div className={`${styles.project} ${styles.project3}`}></div>
+        <div className={styles.projectInfoContainer}>
           <h1>Food Order App</h1>
           <p>React food order app. Select your items and procees to checkout.</p>
           <div className={styles.buttonContainer}>
