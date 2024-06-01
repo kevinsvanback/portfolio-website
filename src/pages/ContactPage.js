8179f93efcdb574cd93ef6c902e02d6b7e52fee9
Kevin Svanbäck <kevin.svanback@gmail.com>
2024-06-01 23:22:12 +0200

    Update expertise styling

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,19 +1,10 @@
-import { faChevronDown, faChevronUp, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react';
+import React from 'react';
 import arrow from '../images/arrow.png';
 import styles from './ContactPage.module.css';
 
 const ContactPage = () => {
-  const [emailIsShown, setEmailIsShown] = useState(false);
-  const [phoneIsShown, setPhoneIsShown] = useState(false);
-
-  const showEmailHandler = () => {
-    setEmailIsShown(!emailIsShown);
-  };
-  const showPhoneHandler = () => {
-    setPhoneIsShown(!phoneIsShown);
-  };
 
   return (
     <>
@@ -21,32 +12,24 @@ const ContactPage = () => {
         <div className={styles.contactContainer}>
           <div className={styles.contactInfo}>
             <div className={styles.buttonAndHeadingContainer}>
-              <div className={styles.chevronButton} onClick={showEmailHandler}>
-                <FontAwesomeIcon className={styles.chevronIcon} icon={!emailIsShown ? faChevronDown : faChevronUp} />
-              </div>
               <div className={styles.contactHeading}>
                 <h3>Email</h3>
                 <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
               </div>
             </div>
             <div className={styles.contactContent}>
-              {!emailIsShown && <p className={styles.invisible}>kevin.svanback@gmail.com</p>}
-              {emailIsShown && <p>kevin.svanback@gmail.com</p>}
+              <p>kevin.svanback@gmail.com</p>
             </div>
           </div>
           <div className={styles.contactInfo}>
             <div className={styles.buttonAndHeadingContainer}>
-              <div className={styles.chevronButton} onClick={showPhoneHandler}>
-                <FontAwesomeIcon className={styles.chevronIcon} icon={!phoneIsShown ? faChevronDown : faChevronUp} />
-              </div>
               <div className={styles.contactHeading}>
                 <h3>Phone</h3>
                 <FontAwesomeIcon className={styles.icon} icon={faPhone} />
               </div>
             </div>
             <div className={styles.contactContent}>
-              {!phoneIsShown && <p className={styles.invisible}>076-3060772</p>}
-              {phoneIsShown && <p>076-3060772</p>}
+              <p>076-3060772</p>
             </div>
           </div>
         </div>
